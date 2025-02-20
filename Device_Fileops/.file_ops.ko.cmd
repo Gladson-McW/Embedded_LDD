00f6fc9fc114a6cfe20e9a205237d2ce7702d28e
Gladson K <gladson.kalanjiadurai@multicorewareinc.com>
2025-02-20 11:27:43 +0530

    --------------------------- Function added : file_operations implementation File name : file_ops.c changes:  initialized and loaded all kernel functions and objects change Id : Gladson K gladson.kalanjiadurai@multicorewareinc.com ----------------.

---
+++ Device_Fileops/.file_ops.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/mcw_cbe/Embedded_LDD/Device_Fileops/file_ops.ko := ld -r -EL  -maarch64elf -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T arch/arm64/module.lds -o /home/mcw_cbe/Embedded_LDD/Device_Fileops/file_ops.ko /home/mcw_cbe/Embedded_LDD/Device_Fileops/file_ops.o /home/mcw_cbe/Embedded_LDD/Device_Fileops/file_ops.mod.o;  true
