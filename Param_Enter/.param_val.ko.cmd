2dff36542e157e5d28d2b6b7512dd9892ea20043
Gladson K <gladson.kalanjiadurai@multicorewareinc.com>
2025-02-20 11:38:05 +0530

    --------------------------- Function added : Input String Integer Integer Array to Kernel File name : param_val.c changes:  initialized and loaded all kernel functions and objects change Id : Gladson K gladson.kalanjiadurai@multicorewareinc.com ----------------

---
+++ Param_Enter/.param_val.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/mcw_cbe/Embedded_LDD/Param_Enter/param_val.ko := ld -r -EL  -maarch64elf -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T arch/arm64/module.lds -o /home/mcw_cbe/Embedded_LDD/Param_Enter/param_val.ko /home/mcw_cbe/Embedded_LDD/Param_Enter/param_val.o /home/mcw_cbe/Embedded_LDD/Param_Enter/param_val.mod.o;  true
