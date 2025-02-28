4c3b9f425cecf2bb42aa45eeedcc0e8e052d75ac
Gladson K <gladson.kalanjiadurai@multicorewareinc.com>
2025-02-28 11:47:22 +0530

    --------------------------- Function added : Waitqueue Static and IPI Implementation File name : wait_stat.c  changes : Waitqueue Static Implementation change Id : Gladson K gladson.kalanjiadurai@multicorewareinc.com

---
+++ work_stat/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/mcw_cbe/Embedded_LDD/work_stat/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/mcw_cbe/Embedded_LDD/work_stat/Module.symvers -T /home/mcw_cbe/Embedded_LDD/work_stat/modules.order -i Module.symvers -e 
---
+++ work_stat/work_stat.mod
@@ -0,0 +1,1 @@
+/home/mcw_cbe/Embedded_LDD/work_stat/work_stat.o
